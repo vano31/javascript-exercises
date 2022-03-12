ef8cc10e916bf2171e229c2ddf3dcd7ca441745e
vano31 <vanosike31@gmail.com>
2022-03-11 23:52:29 -0500

    Complete 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    const l = str.length;
+    let newStr = str.slice(-1);
+    for (i = 0; i > -l; i--) {
+        let newLtr = str.slice((i - 1), i);
+        newStr += newLtr;
+    }
+    return newStr;
 };
 
 // Do not edit below this line
