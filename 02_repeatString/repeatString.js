778ded4e4653ecc5f3fed1fe4f7ab83a83a3826a
vano31 <vanosike31@gmail.com>
2022-03-11 22:39:54 -0500

    Complete 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,21 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let newString = string;
+    if (num === 0) {
+        return ``;
+    } else if (num < 0) {
+        return `ERROR`;
+    } else if (string === ``) {
+        return ``;
+    }
+    for (let i = 1; i < num; i++) {
 
+        newString = string + newString;
+    }
+    return newString;
 };
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
+
