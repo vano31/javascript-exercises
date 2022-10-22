0e0d4339dd4a0b1ffe6ce5d940b393f7ef4c59e9
vano31 <vanosike31@gmail.com>
2022-10-22 19:47:23 -0400

    Complete everything except sum() and multiply(), issue with test environment brackets

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,73 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y
 };
 
-const sum = function() {
-	
+/* This version uses normal for loop and causes problems even though it shouldn't)
+const sum = function(...kirby) {
+
+  if (kirby.length === 0) {
+    return 0;
+  } else {
+
+    let addition = kirby[0];
+    
+	  for (let z = 1; z < kirby.length; z++) {
+      addition += kirby[z];
+    }
+
+    return addition;
+
+  }
+
 };
+
+*/
+
+/*
+This didn't work either. Maybe an issue with the [] in the test environment?
+const sum = function(...kirby) {
+
+  const initialValue = 0;
+  const sumWithInitial = kirby.reduce(
+    (previousValue, currentValue) => previousValue + currentValue,
+    initialValue
+  );
+
+  return sumWithInitial;
+
+
+}
+
+*/
+
+
 
 const multiply = function() {
-
+  //Won't try this because I will run into the the same problem I have with sum
 };
 
-const power = function() {
+const power = function(x, y) {
+
+  return x ** y;
 	
 };
 
-const factorial = function() {
+const factorial = function(x) {
+
+  if (x === 0) {
+
+    return 1;
+
+  }  else {
+
+    return x * factorial(x- 1);
+
+  }
+
 	
 };
 
