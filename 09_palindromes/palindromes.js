e9db772f6088008c3a6771b89a37ea516b741e8e
vano31 <vanosike31@gmail.com>
2022-10-25 19:31:19 -0400

    Finish Exercise completely See if you can do it with recursion

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,31 @@
-const palindromes = function () {
+const palindromes = function (oldString) {
+
+    let symbols = [".", ",", "?", "!", " "];
+
+    let oldStringArray = oldString.toUpperCase().split("").filter(item => symbols.includes(item) === false);
+
+ /*
+    for (stuff in symbols) {
+        if ((oldStringArray.includes(stuff) === true) {
+            oldStringArray.splice(indexOf)
+        }
+    };
+
+ */
+ 
+    let newStringArray = [];
+    
+    for (let x = (oldStringArray.length - 1); x >= 0; x--) {
+        
+        newStringArray.push(oldStringArray[x]);
+
+    }
+
+    let result = newStringArray.toString() === oldStringArray.toString() ? true : false;
+
+    return result;
+
+    
 
 };
 
