dec85bcc59f2f60cd6c14708d21ebcf6ff1a87de
vano31 <vanosike31@gmail.com>
2022-10-25 21:44:35 -0400

    Finish Fibonacci Exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(x) {
+
+    let number = Number(x);
+    let innerfunction = function(number) {
+
+
+        if (number <= 0) {
+            return 0;
+        }   else if (number === 1) {
+            return 1;
+        }   else {
+            return innerfunction(number - 2) + innerfunction(number - 1);
+        }
+
+    }
+
+    let answer = innerfunction(number);
+    if (answer <= 0) {
+        return "OOPS"
+    }   else {
+        return answer;
+    }
+    
 
 };
 
